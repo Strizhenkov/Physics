356ccd1c2660cedac1594f709d2b27006c4e59ca
Strizhenkov <87713684+Strizhenkov@users.noreply.github.com>
2025-02-13 11:44:40 +0300

    Add files via upload

---
+++ CarCrash.pas
@@ -0,0 +1,132 @@
+﻿uses Graph3D; 
+
+const speed = 4;
+const dist = 50;
+const safe_level = 1; // 0 - без всего
+                      // 1 - только ремень безопасности
+                      // 2 - ремень и подушка безопасности
+
+begin
+  View3D.ShowGridLines := False;
+  Camera.Position := P3D(60,25,25);
+  
+  if safe_level = 2 then begin
+    var b1 := Box(0, 0, 1, 6, 2, 1, Colors.Black);
+    var b2 := Box(2, 0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b3 := Box(2, -0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b4 := Box(2.875, 0, 2, 0.25, 1.5, 1, Colors.Blue);
+    var b5 := Box(1.125, 0, 2.5, 0.25, 1.5, 2, Colors.Blue);
+    var b6 := Box(2, 0, 3.5625, 2, 2, 0.125, Colors.Red);
+    var s1 := Sphere(1.75, 0, 3.25, 0.25, Colors.Brown);
+    var b7 := Box(1.75, 0, 2.5, 0.4, 0.4, 1, Colors.Brown);
+    var block := Box(dist + 4, 0, 1, 2, 2, 2);
+    var c1 := Prism(1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c2 := Prism(-1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c3 := Prism(1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var c4 := Prism(-1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var t1 := Torus(1.75, 0, 2.75, 0.55, 0.1, Colors.Black);
+    var car := Group(b1,b2,b3,b4,b5,b6,s1,b7,t1,c1,c2,c3,c4);
+    var ac := car.AnimMoveByX(dist,dist/speed);
+    var a1 := c1.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a2 := c2.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a3 := c3.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a4 := c4.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a := ac*a1*a2*a3*a4;
+    a.Begin;
+    sleep(round(dist/speed*1000));
+    if speed >= 5 then begin
+      var e1 := Ellipsoid(dist+2.6, 0, 2.5, 0.25, 0.5, 0.25, Colors.White);
+      var driver := Group(t1,s1,b7);
+      driver.MoveTo(dist-1,0,2);
+      var p := driver.AnimRotate(OrtY,45,0.1);
+      p.Begin;
+    end;  
+  end;
+  
+  if safe_level = 1 then begin
+    var b1 := Box(0, 0, 1, 6, 2, 1, Colors.Black);
+    var b2 := Box(2, 0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b3 := Box(2, -0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b4 := Box(2.875, 0, 2, 0.25, 1.5, 1, Colors.Blue);
+    var b5 := Box(1.125, 0, 2.5, 0.25, 1.5, 2, Colors.Blue);
+    var b6 := Box(2, 0, 3.5625, 2, 2, 0.125, Colors.Red);
+    var s1 := Sphere(1.75, 0, 3.25, 0.25, Colors.Brown);
+    var b7 := Box(1.75, 0, 2.5, 0.4, 0.4, 1, Colors.Brown);
+    var block := Box(dist + 4, 0, 1, 2, 2, 2);
+    var c1 := Prism(1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c2 := Prism(-1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c3 := Prism(1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var c4 := Prism(-1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var t1 := Torus(1.75, 0, 2.75, 0.55, 0.1, Colors.Black);
+    var car := Group(b1,b2,b3,b4,b5,b6,s1,b7,t1,c1,c2,c3,c4);
+    var ac := car.AnimMoveByX(dist,dist/speed);
+    var a1 := c1.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a2 := c2.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a3 := c3.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a4 := c4.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a := ac*a1*a2*a3*a4;
+    a.Begin;
+    if (speed >= 5) and (speed < 17) then begin
+      sleep(round(dist/speed*1000)); 
+      var driver := Group(t1,s1,b7);
+      driver.MoveTo(dist-1,0,2);
+      var p := driver.AnimRotate(OrtY,45,0.1);
+      p.Begin;
+    end
+    else if speed > 17 then begin
+      sleep(round(dist/speed*1000));
+      var driver := Group(t1,s1,b7);
+      driver.MoveTo(dist+1,0,4);
+      var p := driver.AnimRotate(OrtY,90,0.1);
+      p.Begin;
+    end;
+  end
+  
+  else if safe_level = 0 then begin
+    var b1 := Box(0, 0, 1, 6, 2, 1, Colors.Black);
+    var b2 := Box(2, 0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b3 := Box(2, -0.875, 2.5, 2, 0.25, 2, Colors.Red);
+    var b4 := Box(2.875, 0, 2, 0.25, 1.5, 1, Colors.Blue);
+    var b5 := Box(1.125, 0, 2.5, 0.25, 1.5, 2, Colors.Blue);
+    var b6 := Box(2, 0, 3.5625, 2, 2, 0.125, Colors.Red);
+    var s1 := Sphere(1.75, 0, 3.25, 0.25, Colors.Brown);
+    var b7 := Box(1.75, 0, 2.5, 0.4, 0.4, 1, Colors.Brown);
+    var block := Box(dist + 4, 0, 1, 2, 2, 2);
+    var c1 := Prism(1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c2 := Prism(-1.5, 1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,-90);
+    var c3 := Prism(1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var c4 := Prism(-1.5, -1, 1, 10, 0.5, 1, Colors.White).Rotate(OrtX,90);
+    var car := Group(b1,b2,b3,b4,b5,b6,s1,b7,c1,c2,c3,c4);
+    var ac := car.AnimMoveByX(dist,dist/speed);
+    var a1 := c1.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a2 := c2.AnimRotate(OrtZ,dist*360/2/Pi,dist/speed);
+    var a3 := c3.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a4 := c4.AnimRotate(OrtZ,dist*-360/2/Pi,dist/speed);
+    var a := ac*a1*a2*a3*a4;
+    a.Begin;
+    if speed < 5 then begin
+      sleep(round(dist/speed*1000)); 
+      var driver := Group(s1,b7);
+      driver.MoveTo(dist-1,0,2);
+      var p := driver.AnimRotate(OrtY,45,0.1);
+      p.Begin;
+    end
+    else if (speed >= 5) and (speed <= 17) then begin
+      sleep(round(dist/speed*1000));
+      var driver := Group(s1,b7);
+      driver.MoveTo(dist+1,0,4);
+      var p := driver.AnimRotate(OrtY,90,0.1);
+      p.Begin;
+    end
+    else if speed > 17 then begin
+      sleep(round(dist/speed*1000));
+      var driver := Group(s1,b7);
+      driver.MoveTo(dist+1,0,2);
+      var p1 := driver.AnimRotate(OrtY,90,0.5);
+      var p2 := driver.AnimMoveByX(speed/5,0.5);
+      var p := p1*p2;
+      p.Begin;
+    end;
+  end;
+  
+end.
---
+++ MgO.pas
@@ -0,0 +1,98 @@
+﻿uses Graph3D;
+
+const Models_size = 1;
+
+  //Красные шары - O
+  //Серые шары - Mg
+  //Заряд = 1,6021766208(98)⋅10⁻¹⁹ Кл
+  //Постоянная решетки (нанометры 1⁻⁹) 4.212(нм)
+  //Ионы Mg = 2+ (160*10^−12м)
+  //Ионы O = 2- (60*10^−12м)
+  //Сила взаимодействия между ионами и энергия по закону Кулона:
+  //F=(|q1|*|q2|)/(4*pi*r^2*ε0)
+  //(ε0 - абсолютная диэлектрическая проницаемость среды равна 8,85*10^-12Кл^2/H*м^2)
+  //E=F/ε (ε - диэлектрическая проницаемость среды)
+  
+begin
+  View3D.ShowGridLines := False;
+  
+  
+  
+  var s11 := Sphere(1,1,1,Models_size,Colors.DarkGray);
+  var s12 := Sphere(3,1,1,Models_size/2,Colors.Red);
+  var s13 := Sphere(5,1,1,Models_size,Colors.DarkGray);
+  var s14 := Sphere(1,3,1,Models_size/2,Colors.Red);
+  var s15 := Sphere(3,3,1,Models_size,Colors.DarkGray);
+  var s16 := Sphere(5,3,1,Models_size/2,Colors.Red);
+  var s17 := Sphere(1,5,1,Models_size,Colors.DarkGray);
+  var s18 := Sphere(3,5,1,Models_size/2,Colors.Red);
+  var s19 := Sphere(5,5,1,Models_size,Colors.DarkGray);
+  
+  var s21 := Sphere(1,1,3,Models_size/2,Colors.Red);
+  var s22 := Sphere(3,1,3,Models_size,Colors.DarkGray);
+  var s23 := Sphere(5,1,3,Models_size/2,Colors.Red);
+  var s24 := Sphere(1,3,3,Models_size,Colors.DarkGray);
+  var s25 := Sphere(3,3,3,Models_size/2,Colors.Red);
+  var s26 := Sphere(5,3,3,Models_size,Colors.DarkGray);
+  var s27 := Sphere(1,5,3,Models_size/2,Colors.Red);
+  var s28 := Sphere(3,5,3,Models_size,Colors.DarkGray);
+  var s29 := Sphere(5,5,3,Models_size/2,Colors.Red);
+  
+  var s31 := Sphere(1,1,5,Models_size,Colors.DarkGray);
+  var s32 := Sphere(3,1,5,Models_size/2,Colors.Red);
+  var s33 := Sphere(5,1,5,Models_size,Colors.DarkGray);
+  var s34 := Sphere(1,3,5,Models_size/2,Colors.Red);
+  var s35 := Sphere(3,3,5,Models_size,Colors.DarkGray);
+  var s36 := Sphere(5,3,5,Models_size/2,Colors.Red);
+  var s37 := Sphere(1,5,5,Models_size,Colors.DarkGray);
+  var s38 := Sphere(3,5,5,Models_size/2,Colors.Red);
+  var s39 := Sphere(5,5,5,Models_size,Colors.DarkGray);
+  
+  var t11 := tube(1, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t12 := tube(1, 3, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t13 := tube(1, 5, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t14 := tube(3, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t15 := tube(3, 3, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t16 := tube(3, 5, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t17 := tube(5, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t18 := tube(5, 3, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  var t19 := tube(5, 5, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  
+  var t21 := tube(1, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t21.Rotate(OrtY,90);
+  var t22 := tube(1, 3, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t22.Rotate(OrtY,90);
+  var t23 := tube(1, 5, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t23.Rotate(OrtY,90);
+  var t24 := tube(1, 1, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t24.Rotate(OrtY,90);
+  var t25 := tube(1, 3, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t25.Rotate(OrtY,90);
+  var t26 := tube(1, 5, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t26.Rotate(OrtY,90);
+  var t27 := tube(1, 1, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t27.Rotate(OrtY,90);
+  var t28 := tube(1, 3, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t28.Rotate(OrtY,90);
+  var t29 := tube(1, 5, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t29.Rotate(OrtY,90);
+  
+  var t31 := tube(1, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t31.Rotate(OrtX,270);
+  var t32 := tube(3, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t32.Rotate(OrtX,270);
+  var t33 := tube(5, 1, 1, 4, 0.1, 0.01, Colors.DarkGray);
+  t33.Rotate(OrtX,270);
+  var t34 := tube(1, 1, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t34.Rotate(OrtX,270);
+  var t35 := tube(3, 1, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t35.Rotate(OrtX,270);
+  var t36 := tube(5, 1, 3, 4, 0.1, 0.01, Colors.DarkGray);
+  t36.Rotate(OrtX,270);
+  var t37 := tube(1, 1, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t37.Rotate(OrtX,270);
+  var t38 := tube(3, 1, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t38.Rotate(OrtX,270);
+  var t39 := tube(5, 1, 5, 4, 0.1, 0.01, Colors.DarkGray);
+  t39.Rotate(OrtX,270);
+end.
